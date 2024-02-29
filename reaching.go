cb66286980ca2e7b56bee9249ed3fd9a553d15cf
Sebastian <161750330+Sebastian1832alpha@users.noreply.github.com>
2024-02-29 19:16:38 +0200

    Update reaching.go

--- reaching.go
+++ reaching.go
@@ -5,10 +5,17 @@ func main() {
 		log.Fatal(err)
 	}
 
+
+	func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
+
+	
 
 	domainsQuery := r.URL.Query().Get("teritory")
 	if domainsQuery == "" {
